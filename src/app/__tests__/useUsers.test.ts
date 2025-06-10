2d9aa7a02cb3d1110de0b3936f609888b104ebe2
João Pedro Marques Chaves <jchaves@digitalhouse.com>
2025-06-10 10:43:06 -0300

    fix: build problem fix in test useUsers.test.ts

--- src/app/__tests__/useUsers.test.ts
+++ src/app/__tests__/useUsers.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act, waitFor } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import axios from "axios";
 import { useUsers } from "@/app/hooks/useUsers";
 
